15122b8250414fe1044af073b0ac7efe2ed265ee
sonminh162 <32251649+sonminh162@users.noreply.github.com>
2018-11-16 15:25:21 +0700

    finished

---
+++ src/SearchAlgorithm_AlgorithmComplexityTest.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class SearchAlgorithm_AlgorithmComplexityTest {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the String: ");
+        String inputString = scanner.nextLine();
+        int[] frequentChar = new int[255];
+        for(int i = 0; i < inputString.length();i++){
+            int ascii = (int) inputString.charAt(i);
+            frequentChar[ascii]+=1;
+        }
+        int max = 0;
+        char character = (char) 255;
+        for(int i = 0;i < 255;i++){
+            if(frequentChar[i]>max){
+                max = frequentChar[i];
+                character = (char) i;
+            }
+        }
+        System.out.println("The most appearing letter is: '"+character +"'with a frequency of "+max +" times");
+    }
+}
